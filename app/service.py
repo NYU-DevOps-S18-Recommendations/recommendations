639cd15be3e8fb9044788c0a6d8dbb0ac9af1704
Jae Hyun <jaewhyun@gmail.com>
2018-05-02 01:26:48 -0400

    adding different query options as well as behave for query

--- app/service.py
+++ app/service.py
@@ -90,15 +90,23 @@ def list_recommendations():
     """ Retrieves a list of recommendations from the database """
     results = []
     product_id = request.args.get('product_id')
+    recommendation_type = request.args.get('recommendation_type')
+    recommended_product_id = request.args.get('recommended_product_id')
+    likes = request.args.get('likes')
     if product_id:
         message, return_code = query_recommendations_by_product_id(product_id)
+    elif recommendation_type:
+        message, return_code = query_recommendations_by_recommendation_type(recommendation_type)
+    elif recommended_product_id:
+        message, return_code = query_recommendations_by_recommended_product_id(recommended_product_id)
+    elif likes:
+        message, return_code = query_recommendations_by_likes(likes)
     else:
         results = Recommendation.all()
         message = [recommendation.serialize() for recommendation in results]
         return_code = HTTP_200_OK
 
     return jsonify(message), return_code
-
 
 def query_recommendations_by_product_id(product_id):
     """ Query a recommendation from the database that have the same product_id """
@@ -110,6 +118,48 @@ def query_recommendations_by_product_id(product_id):
     else:
         message = {'error': 'Recommendation with product_id: \
                     %s was not found' % str(product_id)}
+        return_code = HTTP_404_NOT_FOUND
+
+    return message, return_code
+
+def query_recommendations_by_recommendation_type(recommendation_type):
+    """ Query a recommendation from the database that have the same recommendation type """
+    recommendations = Recommendation.find_by_recommend_type(str(recommendation_type))
+    if len(recommendations) > 0:
+        message = [recommendation.serialize()
+                   for recommendation in recommendations]
+        return_code = HTTP_200_OK
+    else:
+        message = {'error': 'Recommendation with product_id: \
+                    %s was not found' % str(recommendation_type)}
+        return_code = HTTP_404_NOT_FOUND
+
+    return message, return_code
+
+def query_recommendations_by_recommended_product_id(recommended_product_id):
+    """ Query a recommendation from the database that have the same recommended product id """
+    recommendations = Recommendation.find_by_recommend_product_id(int(recommended_product_id))
+    if len(recommendations) > 0:
+        message = [recommendation.serialize()
+                   for recommendation in recommendations]
+        return_code = HTTP_200_OK
+    else:
+        message = {'error': 'Recommendation with product_id: \
+                    %s was not found' % str(recommended_product_id)}
+        return_code = HTTP_404_NOT_FOUND
+
+    return message, return_code
+
+def query_recommendations_by_likes(likes):
+    """ Query a recommendaiton from the database that have the same number of likes """
+    recommendations = Recommendation.find_by_likes(likes)
+    if len(recommendations) > 0:
+        message = [recommendation.serialize()
+                   for recommendation in recommendations]
+        return_code = HTTP_200_OK
+    else:
+        message = {'error': 'Recommendation with product_id: \
+                    %s was not found' % str(likes)}
         return_code = HTTP_404_NOT_FOUND
 
     return message, return_code
