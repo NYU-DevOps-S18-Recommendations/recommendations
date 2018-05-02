c70cc931d4fe81659b1a58de95364c6ac6956564
gau820827 <gau820827@gmail.com>
2018-05-02 06:55:19 +0000

    [gau820827] Change the base url

--- features/environment.py
+++ features/environment.py
@@ -5,11 +5,13 @@ import os
 from behave import *
 from selenium import webdriver
 
-BASE_URL = os.getenv('BASE_URL', 'http://localhost:8888')
+# BASE_URL = os.getenv('BASE_URL', 'http://localhost:8888')
+BASE_URL = os.getenv('BASE_URL', 'http://nyu-recommendation-service-s18.mybluemix.net')
+
 
 def before_all(context):
     """ Executed once before all tests """
     context.driver = webdriver.PhantomJS()
-    context.driver.implicitly_wait(10) # seconds
+    context.driver.implicitly_wait(10)  # seconds
     context.driver.set_window_size(1120, 550)
     context.base_url = BASE_URL
